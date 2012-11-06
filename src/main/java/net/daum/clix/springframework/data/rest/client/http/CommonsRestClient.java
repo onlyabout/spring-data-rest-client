7f08bfde79dafb64b4096361792624cf651586b5
Jongjin Han <84june@gmail.com>
2012-11-06 17:48:43 +0900

    Apply PoolingClientConnectionManager

--- src/main/java/net/daum/clix/springframework/data/rest/client/http/CommonsRestClient.java
+++ src/main/java/net/daum/clix/springframework/data/rest/client/http/CommonsRestClient.java
@@ -1,16 +1,19 @@
 package net.daum.clix.springframework.data.rest.client.http;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.Map;
 
 import net.daum.clix.springframework.data.rest.client.json.JacksonJsonSerializer;
 import net.daum.clix.springframework.data.rest.client.json.JsonSerializer;
+import net.daum.clix.springframework.data.rest.client.util.RestUrlUtil;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpMessage;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -22,17 +25,22 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceSupport;
+import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
-public class CommonsRestClient extends RestClientBase {
-
-	private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
+public class CommonsRestClient extends RestClientBase implements DisposableBean {
 
 	private HttpClient client;
 
@@ -43,22 +51,24 @@ public class CommonsRestClient extends RestClientBase {
 	public CommonsRestClient(String restServerUrl) {
 		super(restServerUrl);
 
-		// SchemeRegistry schemeRegistry = new SchemeRegistry();
-		// schemeRegistry.register(
-		// new Scheme("http", 8080, PlainSocketFactory.getSocketFactory()));
-		// schemeRegistry.register(
-		// new Scheme("https", 443, SSLSocketFactory.getSocketFactory()));
-		// PoolingClientConnectionManager cm = new
-		// PoolingClientConnectionManager(schemeRegistry);
-		//
-		// cm.setMaxTotal(100);
-		// // Increase default max connection per route to 20
-		// cm.setDefaultMaxPerRoute(20);
-		// // Increase max connections for localhost:80 to 50
-		// HttpHost localhost = new HttpHost("localhost", 8080);
-		// cm.setMaxPerRoute(new HttpRoute(localhost), 50);
+		URL url = null;
+		try {
+			url = new URL(RestUrlUtil.normalize(restServerUrl));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 
-		this.client = new DefaultHttpClient();
+		Assert.notNull(url);
+
+		SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(new Scheme(url.getProtocol(), url.getPort(), PlainSocketFactory.getSocketFactory()));
+
+		PoolingClientConnectionManager cm = new PoolingClientConnectionManager(schemeRegistry);
+		cm.setMaxTotal(200);
+		cm.setDefaultMaxPerRoute(20);
+		cm.setMaxPerRoute(new HttpRoute(new HttpHost(url.getHost(), url.getPort())), 50);
+
+		this.client = new DefaultHttpClient(cm);
 		this.defaultHeader = new BasicHeader("accept", "application/json");
 
 		this.jsonSerializer = new JacksonJsonSerializer();
@@ -136,32 +146,18 @@ public class CommonsRestClient extends RestClientBase {
 
 	private byte[] getBody(HttpResponse httpResponse) {
 
+		byte[] bytes = null;
+		HttpEntity entity = httpResponse.getEntity();
+
 		try {
-
-			if (HttpStatus.SC_OK != httpResponse.getStatusLine().getStatusCode()) {
-				return null;
-			}
-
-			InputStream is = httpResponse.getEntity().getContent();
-			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-			int nRead;
-			// byte[] data = new byte[16384];
-			byte[] data = new byte[DEFAULT_BUFFER_SIZE];
-
-			while ((nRead = is.read(data, 0, data.length)) != -1) {
-				buffer.write(data, 0, nRead);
-			}
-
-			is.close();
-			buffer.flush();
-			EntityUtils.consumeQuietly(httpResponse.getEntity());
-
-			return buffer.toByteArray();
+			if (HttpStatus.SC_OK == httpResponse.getStatusLine().getStatusCode())
+				bytes = EntityUtils.toByteArray(entity);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		return null;
+		EntityUtils.consumeQuietly(entity);
+		return bytes;
 	}
 
 	private HttpResponse execute(HttpUriRequest req) {
@@ -180,6 +176,10 @@ public class CommonsRestClient extends RestClientBase {
 	private HttpMessage setDefaultHeader(HttpMessage httpMessage) {
 		httpMessage.setHeader(defaultHeader);
 		return httpMessage;
+	}
+
+	public void destroy() throws Exception {
+		this.client.getConnectionManager().shutdown();
 	}
 
 }
